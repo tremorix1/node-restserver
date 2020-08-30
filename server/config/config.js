8872455b918444eb71fa41a2c65d8aa9fe2f5990
Carlos <cpreztello@gmail.com>
2020-08-30 19:29:57 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'dev'){
   urlDB = 'mongodb://localhost:27017/cafe';
 }
 else{
-  urlDB = 'mongodb+srv://tremorix1:CkQT3MSUJTJBW2MV@cluster0.fqksy.mongodb.net/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
